a96694bade3501196db4eab9dec7c5da75da8d9c
adamSeidel <adamSeidel@users.noreply.github.com>
2023-01-18 14:19:13 +0000

    Added number of entries for each athlete to the home page

--- client/client.js
+++ client/client.js
@@ -1,19 +1,3 @@
-// function listAthletes() {
-//     let athletes = document.querySelector("#athleteList ul");
-
-//     let newAthlete = document.createElement('li')
-//     newAthlete.setAttribute('class', 'list-group-item list-group-item-action d-flex justify-content-between align-items-center');
-//     newAthlete.innerHTML = 'David Boam'
-
-//     let spanElement = document.createElement('span')
-//     spanElement.setAttribute('class', "badge bg-primary rounded-pill")
-//     spanElement.innerHTML = 10
-
-//     newAthlete.appendChild(spanElement)
-
-//     athletes.appendChild(newAthlete);
-// }
-
 const endpointRoot = 'http://127.0.0.1:8090/';
 
 async function listAthletes() {
@@ -30,7 +14,9 @@ async function listAthletes() {
 
         let spanElement = document.createElement('span')
         spanElement.setAttribute('class', "badge bg-primary rounded-pill")
-        spanElement.innerHTML = 10
+        let numberOfRacesRequest = await fetch(endpointRoot + "athlete/numberOfRaces/" + athlete)
+        let numberOfRaces = await numberOfRacesRequest.text();
+        spanElement.innerHTML = numberOfRaces + ' entries';
 
         newAthlete.appendChild(spanElement)
 
